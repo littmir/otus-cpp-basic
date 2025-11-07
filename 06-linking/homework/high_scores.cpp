cd434b66dac24734c4da382ddadf5b415fc10fc2
Irina Shishkina <shishkinais@ya.ru>
2025-11-07 15:55:31 +0700

    hw: вывод таблицы рекордов вынесен в отдельную функцию
    Вывод таблицы рекордов вынесен в отдельную функцию для того, чтобы
    далее было удобно обрабатывать параметр -table.

--- 06-linking/homework/high_scores.cpp
+++ 06-linking/homework/high_scores.cpp
@@ -4,6 +4,35 @@
 #include <iostream>
 
 void
+high_scores_ouput(game_preferences &game_prefs) {
+  std::ifstream input_file{game_prefs.high_scores_filename, std::ios_base::in};
+  if (!input_file.is_open()) {
+   std::cout << "Failed to open file for read: " << game_prefs.high_scores_filename << "!\n";
+   return;
+  }
+
+  std::cout << "High scores table: \n";
+  std::cout << "Player_name "<< '\t' << "High_score";
+  std::string player_name {};
+  int high_score = 0;
+  while (true) {
+   input_file >> player_name;
+   input_file >> high_score;
+   input_file.ignore();
+   if (input_file.fail()) {
+    break;
+   }
+
+   std::cout << "\n" << player_name << '\t' << high_score;
+   if (player_name == game_prefs.player_name) {
+      std::cout << " <-- is you :)";
+   }
+  }
+  std::cout << "\n";
+  input_file.close();
+}
+
+void
 process_high_score(game_preferences &game_prefs) {
   std::ofstream output_file{game_prefs.high_scores_filename, std::ios_base::app};
   if (!output_file.is_open()) {
@@ -16,26 +45,5 @@ process_high_score(game_preferences &game_prefs) {
   output_file << "\n";
   output_file.close();
 
-  std::ifstream input_file{game_prefs.high_scores_filename, std::ios_base::in};
-  if (!input_file.is_open()) {
-   std::cout << "Failed to open file for read: " << game_prefs.high_scores_filename << "!\n";
-   return;
-  }
-
-  std::cout << "High scores table: \n";
-  std::cout << "Player_name "<< '\t' << "High_score";
-  std::string player_name;
-  int high_score = 0;
-  while (true) {
-   input_file >> player_name;
-   input_file >> high_score;
-   input_file.ignore();
-   if (input_file.fail()) {
-    break;
-   }
-
-   std::cout << "\n" << player_name << '\t' << high_score;
-  }
-  std::cout << " <-- is you :)\n";
-  input_file.close();
+  high_scores_ouput(game_prefs);
 }
--- 06-linking/homework/high_scores.h
+++ 06-linking/homework/high_scores.h
@@ -6,4 +6,7 @@ struct game_preferences;
 void
 process_high_score(game_preferences &game_prefs);
 
+void
+high_scores_ouput(game_preferences &game_prefs);
+
 #endif // HIGH_SCORES_H
