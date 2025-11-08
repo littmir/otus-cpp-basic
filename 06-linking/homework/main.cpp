littmir/otus-cpp-basic

commit 6e162a8266742e0e1158f827cbb037e5af963746

06-linking-hw: parse_args() added

06-linking/homework/main.cpp
@@ -1,0 +1,72 @@
+#include <ctime>
+#include <iostream>
+#include <string>
+
+struct input_args
+{
+  unsigned int max_number_value;
+  int level;
+  bool table;  
+};
+
+void
+parse_args(int argc, char **argv, input_args &in_args) {
+  
+  const int first_arg_pos = 1;
+  if (argc <= first_arg_pos) {
+    return;
+  }
+
+  enum arg {
+    IS_MAX,
+    IS_LEVEL,
+    IS_TABLE,
+    NOT_ARG
+  } is_arg {NOT_ARG};
+  
+  for (int i = first_arg_pos; i < argc; ++i) {
+    
+    std::string argv_value = argv[i];
+    
+    if (argv_value == "-max") {
+      is_arg = IS_MAX;
+      continue;
+    } else if (argv_value == "-level") {
+      is_arg = IS_LEVEL;
+    } else if (argv_value == "-table") {
+      is_arg = IS_TABLE;
+    }
+
+    switch (is_arg)
+    {
+      case IS_MAX: {
+        auto value = std::stoi(argv[i]);
+        if (value > 0) {
+          in_args.max_number_value = value;
+        }
+        is_arg = NOT_ARG;
+        break;
+      }
+
+      case IS_LEVEL:
+        is_arg = NOT_ARG;
+        break;
+    
+      case IS_TABLE:
+        is_arg = NOT_ARG;
+        break;
+      
+        default:
+          std::cout << "Wrong input value: " << argv_value << "!\n";
+        break;
+    }
+  }
+}
+
+int
+main(int argc, char **argv) {
+  input_args in_args {.max_number_value = 100, .level = 0, .table = false};
+  parse_args(argc, argv, in_args);
+
+  return 0;
+}

06-linking/homework/CMakeLists.txt
@@ -1,0 +1,9 @@
+cmake_minimum_required(VERSION 3.5)
+
+set(PROJ_NAME guess-number)
+project(${PROJ_NAME})
+
+add_executable(
+	${PROJ_NAME}
+	main.cpp
+)

commit 5f4197f279bdc64e498b0588a478fde2dcdab91d

hw: исправлена parse_args()
Исправлена parse_args(), ранее не обрабатывались отрицательные значения
аргумента -max.

fixes: 6e162a8266742e0e1158f827cbb037e5af963746

06-linking/homework/main.cpp
@@ -68,7 +68,7 @@ parse_args(int argc, char **argv, input_args &in_args) {
     {
       case IS_MAX: {
         if (is_next_value) {
-          unsigned int value = std::atoi(argv[i]);
+          int value = std::stoi(argv_value);
           if (value > 0) {
             in_args.max_number_value = value;
             std::cout << "Using " << value << " as max value...\n";
@@ -84,7 +84,7 @@ parse_args(int argc, char **argv, input_args &in_args) {
 
       case IS_LEVEL: {
         if (is_next_value) {
-          unsigned int value = std::atoi(argv[i]);
+          int value = std::atoi(argv[i]);
           bool is_new_max_value = true;
           if (value > 0) {
             switch (value) {